9714fa95361039ab584bf0e907a08f4f93c588cf
cksgnl0514 <cksgnl0514@gmail.com>
2022-08-08 14:29:44 +0900

    1.0 Initialization

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/compat/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDOASDq5LL7HZu-lEIti5BRmTXpV5r3pwQ",
+    authDomain: "nwitter-dcbcd.firebaseapp.com",
+    projectId: "nwitter-dcbcd",
+    storageBucket: "nwitter-dcbcd.appspot.com",
+    messagingSenderId: "735974962010",
+    appId: "1:735974962010:web:9af435bac693ea57aff963",
+    measurementId: "G-0MWX717SNS"
+  };
+
+  export default firebase.initializeApp(firebaseConfig);
